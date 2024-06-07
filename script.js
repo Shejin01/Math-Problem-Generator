b156f6da1d4e9a2721efcc1e4c3f3e4b4e5c5854
UsernameRedacted <5sfamilykolliyath@gmail.com>
2024-06-07 21:38:35 +0530

    Basic Things Are Working

--- script.js
+++ script.js
@@ -3,33 +3,54 @@ MathJax = {
 }
 
 function createProblem() {
-	var textP = document.createElement("p");
-	textP.classList.add("problem");
+	var div = document.createElement("div");
+	var problem = document.createElement("p");
+	var answerBox = document.createElement("input")
 	var operation = document.getElementById("operation");
+
+	div.classList.add("question")
+	problem.classList.add("problem");
+	answerBox.classList.add("answerBox");
+	answerBox.setAttribute("type", "number");
 
 	let a = Math.floor(Math.random() * 20);
 	let b = Math.floor(Math.random() * 20);
 
 	if (operation.value == '+') {
-		textP.innerHTML = String.raw`\[${a} + ${b} = ${a+b}\]`;
+		problem.innerHTML = String.raw`\[${a} + ${b} = \]`;
+		problem.value = a+b;
 	}
 	if (operation.value == '-') {
-		textP.innerHTML = String.raw`\[${a} - ${b} = ${a-b}\]`;
+		problem.innerHTML = String.raw`\[${a} - ${b} = \]`;
+		problem.value = a-b;
 	}
 	if (operation.value == '*') {
-		textP.innerHTML = String.raw`\[${a} \times ${b} = ${a*b}\]`;
+		problem.innerHTML = String.raw`\[${a} \times ${b} = \]`;
+		problem.value = a*b;
 	}
 	if (operation.value == '/') {
-		textP.innerHTML = String.raw`\[\frac{${a}}{${b}} = ${a/b}\]`;
+		problem.innerHTML = String.raw`\[\frac{${a}}{${b}} = \]`;
+		problem.value = Math.round(a/b * 100) / 100;
 	}
 	
-	document.body.appendChild(textP);
+	div.appendChild(problem);
+	div.appendChild(answerBox);
+	document.body.appendChild(div);
 	MathJax.typeset();
 }
 
 function clearProblems() {
-	let problems = document.getElementsByClassName("problem");
-	while (problems.length > 0) {
-		problems[0].remove();
+	let questions = document.getElementsByClassName("question");
+	while (questions.length > 0) questions[0].remove();
+}
+
+function checkAnswers() {
+	let questions = document.getElementsByClassName("question");
+	for (let i = 0; i < questions.length; i++) {
+		if (questions[i].children[0].value == questions[i].children[1].value) {
+			questions[i].children[0].style.color = "green";
+		}
+		else questions[i].children[0].style.color = "red";
 	}
+	MathJax.typeset();
 }
